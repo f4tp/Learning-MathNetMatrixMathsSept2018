ddb5e127638fd20af868e4a990a86441a3cd885b
f4tp <mr_treadwell@live.co.uk>
2018-09-27 15:51:33 +0100

    at 16:09 - read in a matrix using relevant method

--- MathsNetMatrixSept2018/Form1.cs
+++ MathsNetMatrixSept2018/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Data.Text;
 
 namespace MathsNetMatrixSept2018
 {
@@ -15,6 +18,14 @@ namespace MathsNetMatrixSept2018
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //takes a matrix in a text file (1,1),(3,4) and the same underneath.. and reads them in
+            //args for method = file path, booleal true if sparse matrix, string delimiter, boolean true if it has headers
+            Matrix<Complex32> fileMatrix = DelimitedReader.Read<Complex32>(@"D:\My Documents\Paul Docs\Education, training, employment\Job\1 Research\C#\2 Tutorials Vids\3 Odds n Sods\3 Math.Net Vector math\csv.txt", false, ",", false);
+            textBox1.AppendText(fileMatrix.ToString("F2"));
         }
     }
 }
